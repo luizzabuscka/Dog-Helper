54ef0b9b30147a67e91ec496cba46852e6e57ef8
Luiz Zabuscka <luiz.zabuscka@gigigo.com.br>
2017-08-02 17:37:23 -0300

    First commit of the project

---
+++ app/src/main/java/com/luizzabuscka/doghelper/view/Splash.kt
@@ -0,0 +1,13 @@
+package com.luizzabuscka.doghelper.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.luizzabuscka.doghelper.R.layout
+
+class Splash : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(layout.activity_splash)
+  }
+}
